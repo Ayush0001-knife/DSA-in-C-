85ff044e6e645759ac8ccd7ddf8d4ed3bc00048b
Ayush Gautam <ayushstudy647@gmail.com>
2026-01-14 21:38:26 +0530

    Fibonacci Series

---
+++ Loops/Fibonacci-Series.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+      int n;
+      cout << "Enter a number : ";
+      cin >> n;
+
+      int first = 0, second = 1;
+      cout << first << "  " << second << "  ";
+
+      for (int i = 0; i < n; i++)
+      {
+            int third = first + second;
+            cout << third << "  ";
+            first = second;
+            second = third;
+      }
+
+      return 0;
+}
