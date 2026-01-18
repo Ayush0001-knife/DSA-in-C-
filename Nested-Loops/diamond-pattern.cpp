8f740f26d088d60394e14bc7768cde68f115d55b
Ayush Gautam <ayushstudy647@gmail.com>
2026-01-18 15:17:09 +0530

    New

---
+++ Nested-Loops/diamond-pattern.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+      for (int i = 1; i <= 4; i++)
+      {
+            for (int j = 3; j >= 3; j--)
+            {
+                  cout << "    ";
+            }
+            for (int j = 1; j <= i + 1; j++)
+            {
+                  cout << " * ";
+            }
+            for (int j = 3; j >= 3; j--)
+            {
+                  cout << "    ";
+            }
+            cout << endl;
+      }
+      return 0;
+}
