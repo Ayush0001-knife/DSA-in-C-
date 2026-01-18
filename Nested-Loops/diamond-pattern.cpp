9b54ec504d14f075b574e78947d508d479eb033a
Ayush Gautam <ayushstudy647@gmail.com>
2026-01-18 15:39:16 +0530

    Nested-Loops

--- Nested-Loops/diamond-pattern.cpp
+++ Nested-Loops/diamond-pattern.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 int main()
+// we have to fortst 4 rows and in each row we have to print spaces and stars
+// in first row we have to print 3 spaces and 2 stars
+// in second row we have to print 2 spaces and 3 stars
+// in third row we have to print 1 space and 4 stars
+// in fourth row we have to print 0 space and 5 stars
 {
       for (int i = 1; i <= 4; i++)
       {
