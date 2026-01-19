32d8bcbf75497496cfbe62aab5bdd9f6852c6722
Ayush Gautam <ayushstudy647@gmail.com>
2026-01-19 21:00:00 +0530

    Add diamond pattern (DSA practice)

--- Nested-Loops/diamond-pattern.cpp
+++ Nested-Loops/diamond-pattern.cpp
@@ -2,16 +2,25 @@
 using namespace std;
 
 int main()
-// we have to fortst 4 rows and in each row we have to print spaces and stars
-// in first row we have to print 3 spaces and 2 stars
-// in second row we have to print 2 spaces and 3 stars
-// in third row we have to print 1 space and 4 stars
-// in fourth row we have to print 0 space and 5 stars
-{
-      for (int i = 1; i <= 4; i++)
-      {
 
+{
+
+      for (int i = 0; i < 4; i++)
+      {
+            for (int j = 1; j <= 4 - i; j++)
+            {
+                  cout << "   ";
+            }
+            for (int k = 1; k <= (2 * i) + 1; k++)
+            {
+                  cout << " * ";
+            }
+            for (int j = 1; j <= 4 - i; j++)
+            {
+                  cout << "   ";
+            }
             cout << endl;
       }
+
       return 0;
 }
