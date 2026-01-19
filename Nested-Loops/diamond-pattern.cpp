6e1ec8f25a5755c513d6ded66aabe58b80687819
Ayush Gautam <ayushstudy647@gmail.com>
2026-01-19 21:00:00 +0530

    Add diamond pattern (DSA practice)

--- Nested-Loops/diamond-pattern.cpp
+++ Nested-Loops/diamond-pattern.cpp
@@ -5,22 +5,22 @@ int main()
 
 {
 
-      // for (int i = 0; i < 4; i++)
-      // {
-      //       for (int j = 1; j <= 4 - i; j++)
-      //       {
-      //             cout << "   ";
-      //       }
-      //       for (int k = 1; k <= (2 * i) + 1; k++)
-      //       {
-      //             cout << " * ";
-      //       }
-      //       for (int j = 1; j <= 4 - i; j++)
-      //       {
-      //             cout << "   ";
-      //       }
-      //       cout << endl;
-      // }
+      for (int i = 0; i < 4; i++)
+      {
+            for (int j = 1; j <= 4 - i; j++)
+            {
+                  cout << "   ";
+            }
+            for (int k = 1; k <= (2 * i) + 1; k++)
+            {
+                  cout << " * ";
+            }
+            for (int j = 1; j <= 4 - i; j++)
+            {
+                  cout << "   ";
+            }
+            cout << endl;
+      }
 
       for (int i = 1; i <= 4; i++)
       {
