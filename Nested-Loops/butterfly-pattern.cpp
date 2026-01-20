24e563bc8d4ea5757a9bc454d28bdc6469252c42
Ayush Gautam <ayushstudy647@gmail.com>
2026-01-20 21:14:08 +0530

    New

--- Nested-Loops/butterfly-pattern.cpp
+++ Nested-Loops/butterfly-pattern.cpp
@@ -11,6 +11,14 @@ int main()
             {
                   cout << " * ";
             }
+            for (int j = 1; j <= (2 * (n - i)); j++)
+            {
+                  cout << "   ";
+            }
+            for (int j = 1; j <= i; j++)
+            {
+                  cout << " * ";
+            }
             cout << endl;
       }
 
