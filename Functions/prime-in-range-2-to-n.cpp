8f6484ecd5317c5f6bcc3a1c9e56267cb6164f3d
Ayush Gautam <ayushstudy647@gmail.com>
2026-01-22 19:22:59 +0530

    New

---
+++ Functions/prime-in-range-2-to-n.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void printPrime(int n)
+{
+
+      for (int i = 3; i < n; i++)
+      {
+            bool isPrime = true;
+            for (int j = 2; j * j <= i; j++)
+            {
+                  if (i % j == 0)
+                  {
+                        isPrime = false;
+                        continue;
+                  }
+            }
+            if (isPrime)
+            {
+                  cout << i << " ";
+            }
+      }
+}
+
+int main()
+{
+      int n;
+      cout << "Enter a number: ";
+      cin >> n;
+      printPrime(n);
+}
