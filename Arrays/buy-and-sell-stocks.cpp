bdafe5795afe9472c87937ec71f66cfd681d3073
Ayush Gautam <ayushstudy647@gmail.com>
2026-01-27 20:00:00 +0530

    Arrays

--- Arrays/buy-and-sell-stocks.cpp
+++ Arrays/buy-and-sell-stocks.cpp
@@ -17,12 +17,34 @@ int bruteForceApproach(int prices[], int n)
       return max_profit;
 }
 
+// This is The Optimized Brute Force Approach with time complexity O(n^2)
+int optimizedBruteForceApproach(int prices[], int n)
+{
+      int best_buy[n];
+      best_buy[0] = INT16_MAX;
+
+      for (int i = 1; i < n; i++)
+      {
+            best_buy[i] = min(best_buy[i - 1], prices[i - 1]);
+      }
+
+      int max_profit = INT16_MIN;
+
+      for (int i = 0; i < n; i++)
+      {
+            int curr_profit = prices[i] - best_buy[i];
+            max_profit = max(max_profit, curr_profit);
+      }
+      return max_profit;
+}
+
 int main()
 {
       int prices[] = {7, 1, 5, 3, 6, 4};
       int n = sizeof(prices) / sizeof(int);
 
-      int max_profit = bruteForceApproach(prices, n);
+      // int max_profit = bruteForceApproach(prices, n);
+      int max_profit = optimizedBruteForceApproach(prices, n);
 
       cout << "Max Profit : " << max_profit;
       return 0;
