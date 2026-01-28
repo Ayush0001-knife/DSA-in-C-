a63c192a043c212b797c43d24d1665bc68c370cb
Ayush Gautam <ayushstudy647@gmail.com>
2026-01-28 20:00:00 +0530

    Arrays

---
+++ Arrays/reverse-an-array.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void printaArray(int arr[], int n)
+{
+      for (int i = 0; i < n; i++)
+      {
+            cout << arr[i] << " ";
+      }
+}
+
+int main()
+{
+      int arr[] = {5, 4, 3, 9, 2};
+      int n = sizeof(arr) / sizeof(int);
+      int newArr[n];
+
+      for (int i = n - 1; i >= 0; i--)
+      {
+            newArr[n - i - 1] = arr[i];
+      }
+
+      printaArray(newArr, n);
+
+      return 0;
+}
