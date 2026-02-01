11107814713e0bf09578aaf8943630fdc657b0b3
Ayush Gautam <ayushstudy647@gmail.com>
2026-02-01 09:41:33 +0530

    Arrays

--- Arrays/max-subarray-sum.cpp
+++ Arrays/max-subarray-sum.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-// This is The Brute Force Approach with O(n^3)
+// This is The Brute Force Approach with time complexity O(n^3)
 int bruteForceApproach(int arr[], int n)
 {
       int max_sum = INT16_MIN;
@@ -24,13 +24,36 @@ int bruteForceApproach(int arr[], int n)
       return max_sum;
 }
 
+// This is the Optimized Brute Force Approach with time complexity O(n^2)
+int optimizedBruteForceApproach(int arr[], int n)
+{
+      int max_sum = INT16_MIN;
+
+      for (int i = 0; i < n; i++)
+      {
+            int curr_sum = 0;
+            for (int j = i; j < n; j++)
+            {
+                  curr_sum += arr[j];
+                  max_sum = max(max_sum, curr_sum);
+            }
+      }
+      return max_sum;
+}
+
+int kadanesApproach(int arr[], int n)
+{
+}
+
 int main()
 {
       int arr[] = {2, -3, 6, -5, 4, 2};
       // int arr[] = {1, 2, 3, 4, 5};
       int n = sizeof(arr) / sizeof(arr[0]);
 
-      int max_sum = bruteForceApproach(arr, n);
+      // int max_sum = bruteForceApproach(arr, n);
+      // int max_sum = optimizedBruteForceApproach(arr, n);
+      int max_sum = kadanesApproach(arr, n);
 
       cout << max_sum << endl;
       return 0;
