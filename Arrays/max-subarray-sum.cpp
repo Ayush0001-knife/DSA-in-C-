4b511c9a8fb692ca68be8e1a5bc6b29bf1dcc9a6
Ayush Gautam <ayushstudy647@gmail.com>
2026-02-01 10:07:06 +0530

    Arrays

--- Arrays/max-subarray-sum.cpp
+++ Arrays/max-subarray-sum.cpp
@@ -41,8 +41,21 @@ int optimizedBruteForceApproach(int arr[], int n)
       return max_sum;
 }
 
+// This is the Kadane's Algorithm based Approach with time complexity O(n)
 int kadanesApproach(int arr[], int n)
 {
+      int curr_sum = 0, max_sum = INT16_MIN;
+
+      for (int i = 0; i < n; i++)
+      {
+            curr_sum += arr[i];
+            max_sum = max(curr_sum, max_sum);
+            if (curr_sum < 0)
+            {
+                  curr_sum = 0;
+            }
+      }
+      return max_sum;
 }
 
 int main()
