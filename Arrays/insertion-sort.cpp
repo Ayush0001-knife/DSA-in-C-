df6fcfd78ee36ac952115f6a1947e5f663b3b7bf
Ayush Gautam <ayushstudy647@gmail.com>
2026-02-07 09:21:26 +0530

    Arrays

---
+++ Arrays/insertion-sort.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[], int n)
+{
+      for (int i = 0; i < n; i++)
+      {
+            cout << arr[i] << " ";
+      }
+      cout << endl;
+}
+
+void insertionSort(int arr[], int n)
+{
+      for (int i = 1; i < n; i++)
+      {
+            int curr = i;
+            int prev = i - 1;
+
+            while (prev >= 0 && arr[prev] > curr)
+            {
+                  arr[prev + 1] = arr[prev];
+
+                  prev--;
+            }
+      }
+}
+
+int main()
+{
+      int arr[] = {5, 4, 1, 3, 2};
+      int n = sizeof(arr) / sizeof(int);
+
+      insertionSort(arr, n);
+
+      return 0;
+}
