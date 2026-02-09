9d70ed2e061e5bfcefb361733e54998b2033f0ac
Ayush Gautam <ayushstudy647@gmail.com>
2026-02-09 20:00:00 +0530

    Char-Arrays-and-Strings

--- Char-Arrays-and-Strings/reverse-a-char-array.cpp
+++ Char-Arrays-and-Strings/reverse-a-char-array.cpp
@@ -3,5 +3,19 @@ using namespace std;
 
 int main()
 {
+      char word[] = "code";
+      int n = strlen(word);
+
+      int str = 0, end = n - 1;
+
+      while (str < end)
+      {
+            swap(word[str], word[end]);
+            str++;
+            end--;
+      }
+
+      cout << word;
+
       return 0;
 }
