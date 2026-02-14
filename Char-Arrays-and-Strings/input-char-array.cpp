2c62c456ec8adcf871ec6be1f4fba0e39ebccd89
Ayush Gautam <ayushstudy647@gmail.com>
2026-02-14 22:08:42 +0530

    Char-Arrays-and-Strings

---
+++ Char-Arrays-and-Strings/input-char-array.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+      char word[10];
+      cin >> word;
+
+      cout << "Your word was : " << word << endl;
+      cout << "Length of your word : " << strlen(word) << endl;
+
+      return 0;
+}
