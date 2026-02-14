13bf7ee083103004691eeaf417925f3ead76bb3e
Ayush Gautam <ayushstudy647@gmail.com>
2026-02-14 22:18:53 +0530

    Char-Arrays-and-Strings

--- Char-Arrays-and-Strings/input-char-array.cpp
+++ Char-Arrays-and-Strings/input-char-array.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int main()
+void firstMethod()
 {
       char word[10];
       cin >> word;
 
       cout << "Your word was : " << word << endl;
       cout << "Length of your word : " << strlen(word) << endl;
+}
+
+void secondMethod()
+{
+      char word[50];
+      cin.getline(word, 50, '.');
+
+      cout << "Your word was : " << word << endl;
+      cout << "Length of your word : " << strlen(word) << endl;
+}
+
+int main()
+{
+      cout << "This is Done By first Method " << endl;
+      firstMethod();
+
+      cout << endl;
+      cout << endl;
+
+      cout << "This is Done By second Method " << endl;
+      secondMethod();
 
       return 0;
 }
