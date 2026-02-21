9687a7bb2fb59bd337c2b7f88b1757145ed23f56
Ayush Gautam <ayushstudy647@gmail.com>
2026-02-21 16:47:51 +0530

    Vectors

---
+++ Vectors/dynamic-1D-array-allocation.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void funcArr()
+{
+      int size;
+      cin >> size;
+
+      int *arr = new int[size];
+
+      int x = 1;
+      for (int i = 0; i < size; i++)
+      {
+            arr[i] = x;
+            cout << arr[i] << " ";
+            x++;
+      }
+      cout << endl;
+
+      delete[] arr;
+}
+
+int main()
+{
+      funcArr();
+
+      return 0;
+}
