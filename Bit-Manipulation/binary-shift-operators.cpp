2b535a9a321d1b711468f05882ae6f37e745bb41
Ayush Gautam <ayushstudy647@gmail.com>
2026-03-04 20:00:00 +0530

    Bit-Manipulation

---
+++ Bit-Manipulation/binary-shift-operators.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+      int a = 7; // In binary: 0111₹
+
+      // Left Shift ("<<")
+      cout << "a << 2 = " << (a << 2) << endl; // Output: 28 (binary: 0111 << 2 = 11100)
+
+      return 0;
+}
