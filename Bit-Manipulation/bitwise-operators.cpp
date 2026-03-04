c30d3bbc73a0c022eeb222aee7f65e626a127c3a
Ayush Gautam <ayushstudy647@gmail.com>
2026-03-04 20:00:00 +0530

    Bit-Manipulation

--- Bit-Manipulation/bitwise-operators.cpp
+++ Bit-Manipulation/bitwise-operators.cpp
@@ -15,5 +15,9 @@ int main()
       // Bitwise XOR ("^")
       cout << "a ^ b = " << (a ^ b) << endl; // Output: 6 (binary: 0011 ^ 0101 =  0110)
 
+      // Bitwise NOT ("~")
+      cout << "~a = " << (~a) << endl; // Output: -4 (binary: ~0011 = 1100 in two's complement)
+      cout << "~b = " << (~b) << endl; // Output: -6 (binary: ~0101 = 1010 in two's complement)
+
       return 0;
 }
