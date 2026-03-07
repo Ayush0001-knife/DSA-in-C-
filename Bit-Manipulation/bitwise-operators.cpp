7bf4558030cf1949aa569e004c37dd090d37636e
Ayush Gautam <ayushstudy647@gmail.com>
2026-03-07 09:57:05 +0530

    Bit-Manipulation

---
+++ Bit-Manipulation/bitwise-operators.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+      int a = 3;
+      int b = 5;
+
+      // Bitwise AND ("&")
+      cout << "a & b = " << (a & b) << endl; // Output: 1 (binary: 0011 & 0101 = 0000 0001)
+
+      return 0;
+}
