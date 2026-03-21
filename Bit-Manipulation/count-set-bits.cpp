bbbaefcd56a9f6aa20d9142f99baed7a14b7fa99
Ayush Gautam <ayushstudy647@gmail.com>
2026-03-21 09:55:53 +0530

    Bit-Manipulation

---
+++ Bit-Manipulation/count-set-bits.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+      cout << "Enter a Positive integer: ";
+      int n;
+      cin >> n;
+      int count = 0;
+
+      while (n != 0)
+      {
+            if ((n & 1) != 0)
+            {
+                  count += 1;
+            }
+            n = n >> 1;
+      }
+
+      cout << "Count is : " << count;
+
+      return 0;
+}
