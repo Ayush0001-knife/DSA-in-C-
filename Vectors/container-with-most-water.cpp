c8541acfe936712dd91d50a7da4c131cf822588f
Ayush Gautam <ayushstudy647@gmail.com>
2026-03-22 21:09:34 +0530

    Vectors

---
+++ Vectors/container-with-most-water.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+      int height[] = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+      int n = sizeof(height) / sizeof(int);
+
+            return 0;
+}
