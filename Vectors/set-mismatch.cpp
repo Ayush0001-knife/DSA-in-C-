24935a64edc6a144b449d6d4e9820467e33b8895
Ayush Gautam <ayushstudy647@gmail.com>
2026-03-22 17:02:27 +0530

    Vectors

--- Vectors/set-mismatch.cpp
+++ Vectors/set-mismatch.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int main()
+void bruteForceApproach(int nums[], int n)
 {
-      int nums[] = {1, 2, 2, 4};
-      int n = sizeof(nums) / sizeof(nums[0]);
-
       for (int i = 0; i < n; i++)
       {
             if (nums[i] == nums[i + 1])
@@ -17,6 +15,40 @@ int main()
                   cout << "Missing: " << nums[i] + 1 << endl;
             }
       }
+}
+
+int main()
+{
+      int nums[] = {1, 2, 2, 4};
+      int n = sizeof(nums) / sizeof(nums[0]);
+
+      sort(nums, nums + n);
+
+      int duplicate = -1, missing = -1;
+
+      if (nums[0] != 1)
+      {
+            missing = 1;
+      }
+
+      for (int i = 0; i < n - 1; i++)
+      {
+            if (nums[i] == nums[i + 1])
+            {
+                  duplicate = nums[i];
+            }
+            if (nums[i + 1] != nums[i] + 1)
+            {
+                  missing = nums[i] + 1;
+            }
+      }
+
+      if (missing == -1)
+      {
+            missing = n;
+      }
+
+      cout << "[ " << duplicate << "," << missing << " ]" << endl;
 
       return 0;
 }
