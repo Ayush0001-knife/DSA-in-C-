07e324b0d0e9a36a845e985ca1f45ed696dc8c79
Ayush Gautam <ayushstudy647@gmail.com>
2026-04-03 09:54:10 +0530

    OOPS Part-1

---
+++ OOPS/Part-1/destructor.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+class Car
+{
+public:
+      string name;
+      string color;
+      int *mileage;
+
+      Car(string name, string color)
+      {
+            this->name = name;
+            this->color = color;
+            mileage = new int; // Dynamic Allocation
+            *mileage = 12;
+      }
+
+      // Below is the example of Shallow copy constructor
+
+      Car(Car &original)
+      {
+            cout << "Copying .... \n";
+            name = original.name;
+            color = original.color;
+            mileage = original.mileage;
+      }
+
+      // Below is the example of Deep copy constructor
+
+      // Car(Car &original)
+      // {
+      //       cout << "Copying .... \n";
+      //       name = original.name;
+      //       color = original.color;
+      //       mileage = new int; // Dynamic Allocation
+      //       *mileage = *original.mileage;
+      // }
+
+      // This is Custom destructor to delete dynamically allocated memory.
+
+      ~Car()
+      {
+            cout << "Deleteing Dynamically Allocated Memory .... !!" << endl;
+
+            if (mileage != NULL)
+            {
+                  delete mileage;
+                  mileage = NULL;
+            }
+      }
+};
+
+int main()
+{
+      Car c1("Fortuner", "White");
+
+      Car c2(c1); // Here Automatically Copy constructor will be created by compiler and copy c1 values in c2.
+      cout << c2.name << " and " << c2.color << " and " << *c2.mileage << endl;
+      ;
+      *c2.mileage = 10;
+
+      cout << *c1.mileage;
+
+      return 0;
+};
