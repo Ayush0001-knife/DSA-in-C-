53321e81e5335136ca7e07ae6f000eb0f1f0633b
Ayush Gautam <ayushstudy647@gmail.com>
2026-04-09 12:00:00 +0530

    OOPS Part-2

--- OOPS/Part-2/run-time-polymorphism.cpp
+++ OOPS/Part-2/run-time-polymorphism.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 using namespace std;
 
-class Parent
+// Function Overriding
+class Parent1
 {
 public:
       void show()
       {
             cout << "Parent" << endl;
       }
+      virtual void hello()
+      {
+            cout << "Hello World" << endl;
+      }
 };
-
-class Child : public Parent
+class Child1 : public Parent1
 {
 public:
       void show()
@@ -19,10 +23,34 @@ public:
       }
 };
 
+// Virtual Function
+class Parent2
+{
+public:
+      virtual void hello()
+      {
+            cout << "Parent Saying Hello World" << endl;
+      }
+};
+
+class Child2 : public Parent2
+{
+public:
+      void hello()
+      {
+            cout << "Child2 Saying Hello World" << endl;
+      }
+};
+
 int main()
 {
-      Child Obj;
+      Child1 Obj;
       Obj.show();
+
+      Parent2 *ptr; // Pointer to Parent
+      Child2 Obj2;  // Object of Child2
+      ptr = &Obj2;  // Pointer points to the object of Child2 This is called binding parent pointer to the child object or it is called Run Time Binding
+      ptr->hello();
 
       return 0;
 }
