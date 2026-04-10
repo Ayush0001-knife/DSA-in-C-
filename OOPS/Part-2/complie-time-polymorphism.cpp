352a20a5baf4b5d6161d6dfafb82a9c403984361
Ayush Gautam <ayushstudy647@gmail.com>
2026-04-10 06:54:15 +0530

    OOPS Part-2

--- OOPS/Part-2/complie-time-polymorphism.cpp
+++ OOPS/Part-2/complie-time-polymorphism.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+// Function Overloading
 class Print
 {
 public:
@@ -15,12 +16,51 @@ public:
       }
 };
 
+// Operator Overloading
+
+class Complex
+{
+      int real;
+      int imag;
+
+public:
+      Complex(int r, int i)
+      {
+            real = r;
+            imag = i;
+      }
+      void showNum()
+      {
+            cout << real << " + " << imag << "i" << endl;
+      }
+      // Operator Overloading Main logic
+      void operator+(Complex &c2)
+      {
+            int resReal = this->real + c2.real;
+            int resImag = this->imag + c2.imag;
+            // cout << "Result: " << resReal << " + " << resImag << "i" << endl;
+            // cout << endl;
+
+            Complex c3(resReal, resImag);
+            cout << "Result: ";
+            c3.showNum();
+      }
+};
+
 int main()
 {
-      Print Obj;
+      // Function Overloading
+      // Print Obj;
+      // Obj.show(10);
+      // Obj.show("Hello World");
 
-      Obj.show(10);
-      Obj.show("Hello World");
+      // Operator Overloading
+      Complex c1(10, 20);
+      Complex c2(30, 40);
+
+      c1.showNum();
+      c2.showNum();
+      c1 + c2;
 
       return 0;
 }
