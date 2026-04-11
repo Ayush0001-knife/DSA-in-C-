9c7b58c457b957621a83b30849e025daa0981b68
Ayush Gautam <ayushstudy647@gmail.com>
2026-04-11 19:53:40 +0530

    OOPS PART-2

---
+++ OOPS/Part-2/abstraction.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class Shape
+{
+public:
+      virtual void draw() // Pure Virtual Function
+      {
+            throw 0;
+      };
+};
+
+int main()
+{
+      return 0;
+}
