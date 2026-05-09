e5ecdec359930af41639374d4b98fd85f92d4f57
Ayush Gautam <ayushstudy647@gmail.com>
2026-05-09 10:00:00 +0530

    Backtracking

--- Backtracking/n-queens.cpp
+++ Backtracking/n-queens.cpp
@@ -60,25 +60,26 @@ bool isSafe(vector<vector<char>> board, int rowNumber, int j)
       return true;
 }
 
-void nQueens(vector<vector<char>> board, int rowNumber)
+int nQueens(vector<vector<char>> board, int rowNumber)
 {
       int n = board.size();
 
       if (rowNumber == n)
       {
             printBoard(board);
-            return;
+            return 1;
       }
-
+      int count = 0;
       for (int j = 0; j < n; j++)
       {
             if (isSafe(board, rowNumber, j))
             {
                   board[rowNumber][j] = 'Q'; // forwarding in recursion to assign queen to new position
-                  nQueens(board, rowNumber + 1);
+                  count += nQueens(board, rowNumber + 1);
                   board[rowNumber][j] = '.'; // using backtracking to remove the queen from the assigned position so that in next iteration queen can sit in another columnof same row
             }
       }
+      return count; // No of possible solutions at each level
 }
 
 int main()
@@ -86,7 +87,9 @@ int main()
       int n = 4;
       std::vector<vector<char>> board(n, std::vector<char>(n, '.'));
 
-      nQueens(board, 0);
+      int count = nQueens(board, 0); // To track total No of possible solutions
+
+      cout << "Count : " << count << endl;
 
       return 0;
 }
