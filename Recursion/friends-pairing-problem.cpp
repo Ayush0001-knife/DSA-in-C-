ffade89fb103aca3ac86bf8aa7ffffae94852ae4
Ayush Gautam <ayushstudy647@gmail.com>
2026-05-20 10:00:00 +0530

    Recursion

---
+++ Recursion/friends-pairing-problem.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int friendsPairing(int n)
+{
+      if (n <= 2)
+      {
+            return n;
+      }
+      return friendsPairing(n - 1) + (n - 1) * friendsPairing(n - 2);
+}
+
+int main()
+{
+      int n = 3;
+      cout << friendsPairing(n) << endl;
+
+      return 0;
+}
