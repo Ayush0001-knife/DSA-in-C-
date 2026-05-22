e466bfb1973fb2bd90974c7ce914232f6c95851b
Ayush Gautam <ayushstudy647@gmail.com>
2026-05-22 10:00:00 +0530

    Recursion

--- Recursion/binary-strings-problem.cpp
+++ Recursion/binary-strings-problem.cpp
@@ -1,28 +1,49 @@
 #include <iostream>
 using namespace std;
 
-void binaryStrings(int n, int lastPlace, string ans)
+// with last place approach
+// void binaryStrings(int n, int lastPlace, string ans)
+// {
+//       if (n == 0)
+//       {
+//             cout << ans << endl;
+//             return;
+//       }
+//       if (lastPlace != 1)
+//       {
+//             binaryStrings(n - 1, 0, ans + "0");
+//             binaryStrings(n - 1, 1, ans + "1");
+//       }
+//       else
+//       {
+//             binaryStrings(n - 1, 0, ans + "0");
+//       }
+// }
+
+// without last place approach
+void binaryStrings(int n, string ans)
 {
       if (n == 0)
       {
             cout << ans << endl;
             return;
       }
-      if (lastPlace != 1)
+      if (ans[ans.size() - 1] != '1')
       {
-            binaryStrings(n - 1, 0, ans + "0");
-            binaryStrings(n - 1, 1, ans + "1");
+            binaryStrings(n - 1, ans + "0");
+            binaryStrings(n - 1, ans + "1");
       }
       else
       {
-            binaryStrings(n - 1, 0, ans + "0");
+            binaryStrings(n - 1, ans + "0");
       }
 }
 
 int main()
 {
       int n = 3;
-      binaryStrings(n, 0, "");
+      // binaryStrings(n, 0, "");
+      binaryStrings(n, "");
 
       return 0;
 }
