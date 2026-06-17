f30a4a8dba408ba32a9ee777ebd3a2415a6f9907
Ayush Gautam <ayushstudy647@gmail.com>
2026-06-17 06:26:16 +0530

    practice

--- practice.cpp
+++ practice.cpp
@@ -2,17 +2,37 @@
 #include <vector>
 using namespace std;
 
+class Complex
+{
+    int real, imag;
+
+public:
+    Complex(int r, int i)
+    {
+        real = r;
+        imag = i;
+    }
+    void showNum()
+    {
+        cout << real << " + " << imag << "i" << endl;
+    }
+    void operator-(Complex &c2)
+    {
+        int resReal = this->real - c2.real;
+        int resImag = this->imag - c2.imag;
+
+        Complex c3(resReal, resImag);
+        cout << "Result: ";
+        c3.showNum();
+    }
+};
+
 int main()
 {
-    vector<int> vec;
+    Complex c1(10, 20);
+    Complex c2(30, 40);
 
-    for (int i = 0; i < 5; i++)
-    {
-        vec.push_back(i);
-    }
-
-    cout << vec.size() << endl;
-    cout << vec.capacity() << endl;
+    c1 - c2;
 
     return 0;
 }
