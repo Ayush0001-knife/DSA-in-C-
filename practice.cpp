1fbe85480bd5d58cae4656d35707b7fb745b7b7a
Ayush Gautam <ayushstudy647@gmail.com>
2026-07-10 20:52:10 +0530

    practice

--- practice.cpp
+++ practice.cpp
@@ -1,110 +1,18 @@
 #include <iostream>
 using namespace std;
 
-class Node
+int sum(int n)
 {
-public:
-    int data;
-    Node *next;
-
-    Node(int val)
+    if (n == 0)
     {
-        data = val;
-        next = NULL;
+        return 0;
     }
-    ~Node()
-    {
-        if (next != NULL)
-        {
-            delete next;
-            next = NULL;
-        }
-    }
-};
-
-class List
-{
-    Node *head;
-    Node *tail;
-
-public:
-    List()
-    {
-        head = NULL;
-        tail = NULL;
-    }
-    ~List()
-    {
-        if (head != NULL)
-        {
-            delete head;
-            head = NULL;
-        }
-    }
-    void push_front(int val)
-    {
-        Node *newNode = new Node(val);
-
-        if (head == NULL)
-        {
-            head = tail = newNode;
-        }
-        else
-        {
-            Node *temp = head;
-            head = newNode;
-            head->next = temp;
-        }
-    }
-
-    void push_back(int val)
-    {
-        Node *newNode = new Node(val);
-        if (head == NULL)
-        {
-            head = tail = newNode;
-        }
-        else
-        {
-            tail->next = newNode;
-            tail = newNode;
-        }
-    }
-
-    void print_list()
-    {
-        Node *temp = head;
-        while (temp != NULL)
-        {
-            cout << "temp->data";
-            temp = temp->next;
-        }
-    };
-
-    void insert(int pos, int val)
-    {
-        Node *newNode = new Node(val);
-        Node *temp = head;
-
-        if (pos == 0)
-        {
-            head = newNode;
-            head->next = temp;
-        }
-        else
-        {
-            Node *temp = head;
-            for (int i = 0; i < pos - 1; i++)
-            {
-                temp = temp->next;
-            }
-            newNode->next = temp->next;
-            temp->next = newNode;
-        }
-    }
-};
+    return n + sum(n - 1);
+}
 
 int main()
 {
+    cout << sum(5);
+
     return 0;
 }
