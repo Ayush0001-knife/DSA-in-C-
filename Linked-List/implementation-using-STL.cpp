d95be0804a32c69e00bf3c2fb2ae2dc24eee651b
Ayush Gautam <ayushstudy647@gmail.com>
2026-08-02 21:06:02 +0530

    Linked-List

---
+++ Linked-List/implementation-using-STL.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main()
+{
+      list<int> myLL;
+
+      myLL.push_front(1);
+      myLL.push_back(2);
+      myLL.push_back(3);
+
+      return 0;
+}
